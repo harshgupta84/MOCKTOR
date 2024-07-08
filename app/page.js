6c5d20837d89bd9650892af2db15e096f1c79e0e
harshgupta84 <21ucs084@lnmiit.ac.in>
2024-07-08 23:41:59 +0530

    1st commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Header from "./dashboard/_components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
