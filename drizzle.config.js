57ccad250dd47c0c6eed15b21ee4dcfedb1396c3
harshgupta84 <21ucs084@lnmiit.ac.in>
2024-07-10 21:57:23 +0530

    chngs

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-  schema: "./utils/schema.js",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: "postgresql://mocktor_owner:fJLsR7up2CDo@ep-damp-glade-a5oq65sp.us-east-2.aws.neon.tech/mocktor?sslmode=require",
-  },
-};
